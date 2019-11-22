ef7f64b8d5af157af5d961749797e291ba97e19b
Francis KANTE <fkante@e2r11p11.42.fr>
2019-11-22 10:05:53 +0100

    [develop] main changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 13:52:48 by fkante            #+#    #+#             */
-/*   Updated: 2019/11/17 15:55:59 by fkante           ###   ########.fr       */
+/*   Updated: 2019/11/20 18:46:35 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,29 @@
 int		main(int ac, char **av)
 {
 	int32_t ret;
-	int32_t fd;
+	int32_t i;
+	st		push;
 
-	ret = FAILURE;
-	if (ac == 2)
+	if (ac >= 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		ft_printf("fd = %d\n", fd);
-		ret = create_table_a(fd);
-		close(fd);
+		push.size = ac - 1;
+		ret = init_struct(&push, av);
+		ft_printf("s.size = %d\n", push.size);
+		for (i = 0; i < ac - 1; i++)
+			ft_printf("stack a[%d] = %d\tstack b[%d] = %d\n", i, push.a[i], i, push.b[i]);
+//		i = ac;
+//		while (i > 0)
+//		{
+//			swap_stack_a(&push.a[i - 1], &push.a[i]);
+//			i--;
+//		}
+//		push_stack_b(&push);
+//		swap_stack_a(&push.a[0], &push.a[1]);
+//		push_stack_b(&push);
+		rotate_stack_a(&push);
+		ft_printf("\n");
+		for (i = 0; i < ac - 1; i++)
+			ft_printf("stack a[%d] = %d\tstack b[%d] = %d\n", i, push.a[i], i, push.b[i]);
 	}
 	return (ret);
 }
