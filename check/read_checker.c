f90f2f15187cc5893bca55b57920583696a7f809
Francis KANTE <fkante@e2r12p14.42.fr>
2019-11-26 18:23:20 +0100

    [CHECKERSTDIN] working on checker to receive for stdin

--- check/read_checker.c
+++ check/read_checker.c
@@ -6,37 +6,22 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 10:53:16 by fkante            #+#    #+#             */
-/*   Updated: 2019/11/25 15:09:54 by fkante           ###   ########.fr       */
+/*   Updated: 2019/11/26 18:22:29 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int8_t	store_stdin(char **line)
-{
-	static size_t	i;
-	size_t			j;
-	char			**operation;
-
-	if ((operation = NULL) != NULL)
-		i = 0;
-	ft_printf("i = %d\n", i);
-	if (line[0] != NULL)
-		operation[i] = ft_strdup(line[0]);
-	i++;
-	return (0);
-}
 
 int8_t	read_checker()
 {
 	char	*line;
 	int32_t	ret;
 
-	line = NULL;
+	line = 0;
 	ret = FAILURE;
-	while ((ret = get_next_line(1, &line)) > 0)
+	while ((ret = get_next_line(0, &line)) > 0)
 	{
-		store_stdin(&line);
+		ft_putendl_fd(line, 1);
 		ft_strdel(&line);
 	}
 	return (ret);
@@ -44,7 +29,8 @@ int8_t	read_checker()
 
 int		main(int ac, char **av)
 {
-	if (ac > 1)
-		read_checker();
+	(void)ac;
+	(void)av;
+	read_checker();
 	return (0);
 }
