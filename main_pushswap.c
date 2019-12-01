f71e5798134cbc7340cc7ca4a920ae64021490d4
Francis KANTE <fkante@e2r11p11.42.fr>
2019-12-01 13:26:13 +0100

    [develop] start on push swap

--- main_pushswap.c
+++ main_pushswap.c
@@ -6,7 +6,7 @@
 /*   By: amartino <amartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 11:02:18 by amartino          #+#    #+#             */
-/*   Updated: 2019/11/29 12:25:49 by fkante           ###   ########.fr       */
+/*   Updated: 2019/12/01 13:23:00 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,23 @@
 
 int		main(int ac, char **av)
 {
-	// int32_t ret;
-	// int32_t i;
-	// int32_t index;
-	// t_stack	push;
-	//
-	// if (ac >= 2)
-	// {
-	// 	push.size_init = ac - 1;
-	// 	index = ac - 2;
-	// 	ret = init_struct(&push, av);
-	// 	ft_printf("---------------\n| s.size_init = %d |\n---------------\n", push.size_init);
-	// 	for (i = index; i >= 0; i--)
-	// 		ft_printf("stack a[%d] =\t%d\t\t|\tstack b[%d] =\t%d\n", i, push.a[i], i, push.b[i]);
-	// 	ft_printf("-----------------\n");
-	// 	//		i = ac;
-	// 	//		while (i > 0)
-	// 	//		{
-	// 	//			swap_stack_a(&push.a[i - 1], &push.a[i]);
-	// 	//			i--;
-	// 	//		}
-	// 	//		push_stack_b(&push);
-	// 	//		swap_stack_a(&push.a[index - 1], &push.a[index]);
-	// 	for (i = 0; i < push.size_init; i++)
-	// 	{
-	// 		push_stack_b(&push);
-	// 		rotate_stack_a(&push);
-	// 	}
-	// 	//	reverse_rotate_stack_a(&push);
-	// 	ft_printf("-----------------\n");
-	// 	for (i = index; i >= 0; i--)
-	// 		ft_printf("stack a[%d] =\t%d\t\t|\tstack b[%d] =\t%d\n", i, push.a[i], i, push.b[i]);
-	// }
-	// return (ret);
+	t_stack		*s;
+
+	s = NULL;
+	if (ac >= 2)
+	{
+		++av;
+		--ac;
+		s = init_struct(av, ac);
+		if (s == NULL)
+		{
+			ft_printf("Error\n");
+			return (0);
+		}
+		scan_stack_a(s);
+		clean_struct(&s);
+	}
+	else
+		ft_printf("Error\n");
 	return (0);
 }
