8cad23846a6d9914af064dc2fc3b2cd000a5175c
Francis KANTE <fkante@e2r11p13.42.fr>
2020-02-27 16:19:04 +0100

    [ALGO_FRANCIS] protected gnl from dev/zero

--- libft/get_next_line_ps.c
+++ libft/get_next_line_ps.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 16:07:42 by fkante            #+#    #+#             */
-/*   Updated: 2020/02/26 20:03:27 by fkante           ###   ########.fr       */
+/*   Updated: 2020/02/27 16:18:47 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ int		get_next_line_ps(const int fd, char *line)
 	char	buff[2];
 	int8_t	ret;
 	int8_t	count;
+	int32_t	max_buff;
 
 	count = 0;
-	while ((ret = read(fd, buff, 1)) > 0)
+	max_buff = 0;
+	while ((ret = read(fd, buff, 1)) > 0 && max_buff < 30000)
 	{
+		max_buff++;
 		buff[1] = '\0';
 		if (count == 4)
 		{
